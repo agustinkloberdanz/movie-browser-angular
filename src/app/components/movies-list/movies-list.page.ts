842229f0c33d6c7ad59e92cbb53bf48713982e26
agustinkloberdanz <akloberdanz399@gmail.com>
2024-05-30 16:20:18 -0300

    nloquea el boton buscar si el input esta vacio

--- src/app/components/movies-list/movies-list.page.ts
+++ src/app/components/movies-list/movies-list.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonImg, IonCard, IonCardHeader, IonCardSubtitle, IonCardContent, IonCardTitle, IonInput, IonButton, IonAlert, IonToast, IonSearchbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonImg, IonCard, IonCardHeader, IonCardSubtitle, IonCardContent, IonCardTitle, IonInput, IonButton, IonAlert, IonToast, IonSearchbar, IonProgressBar } from '@ionic/angular/standalone';
 import { Movie, MoviesListService } from '../../services/movies-list.service';
 
 @Component({
@@ -9,21 +9,22 @@ import { Movie, MoviesListService } from '../../services/movies-list.service';
   templateUrl: './movies-list.page.html',
   styleUrls: ['./movies-list.page.scss'],
   standalone: true,
-  imports: [IonSearchbar, IonToast, IonAlert, IonButton, IonInput, IonCardTitle, IonCardContent, IonCardSubtitle, IonCardHeader, IonCard, IonImg, IonLabel, IonItem, IonList, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonProgressBar, IonSearchbar, IonToast, IonAlert, IonButton, IonInput, IonCardTitle, IonCardContent, IonCardSubtitle, IonCardHeader, IonCard, IonImg, IonLabel, IonItem, IonList, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class MoviesListPage {
-
   search = ''
   movies: Movie[] = []
   message = ''
 
   constructor(private moviesService: MoviesListService) { }
 
-  handleClick() {
-    if (this.search !== '') {
-      this.getMovies()
-    }
-    else {
+
+  async handleClick() {
+    const movie = this.search
+
+    if (movie && movie.trim() != '') {
+      return this.getMovies()
+    } else {
       this.movies = []
       this.message = 'The movie name is required'
     }
